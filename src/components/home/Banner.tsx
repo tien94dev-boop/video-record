ad643fe57d54c91e4b876dbfaf0eae1d940c7ed0
tran dinh tien <tien94dev@gmail.com>
2026-01-29 20:16:53 +0700

    exam

--- src/components/home/Banner.tsx
+++ src/components/home/Banner.tsx
@@ -1,9 +1,10 @@
+import Image from "next/image"
 
 interface BannerItem {
     statistic: string
     statisticName: string
 }
-interface BannerInterface{
+interface BannerInterface {
     title: string
     des: string
     btnName: string
@@ -11,27 +12,38 @@ interface BannerInterface{
     bannerDesktopUrl: string
     bannerMobileUrl: string
 }
-export default function Banner({ data }: {data: BannerInterface}){
+export default function Banner({ data }: { data: BannerInterface }) {
     const { bannerDesktopUrl, bannerMobileUrl, title, des, btnName, bannerItems } = data
-    return <div className={`bg-[url(/images/banner.jpg)] grid grid-cols-2 gap-4 lg:px-25 md:px-4 px-4 py-25`}>
-        <div className="flex flex-col gap-6">
-            <h2 className="text-[64px] leading-[64px] font-bold">{title}</h2>
-            <p className="text-base leading-5.5">{des}</p>
+    return <div className={`bg-[#F2F0F1] grid grid-cols-2 gap-4 lg:px-25 md:px-4 px-4 md:pt-25 pt-8`}>
+        <div className="flex flex-col gap-6 col-span-2 md:col-span-1">
+            <h2 className="md:text-[64px] md:leading-[64px] text-4xl leading-8 md:font-bold">{title}</h2>
+            <p className="md:text-base leading-5.5 text-sm">{des}</p>
             <div>
-                <div className="w-[210px] h-13 bg-black flex justify-center items-center rounded-full cursor-pointer">
+                <div className="md:w-[210px] w-full h-13 bg-black flex justify-center items-center rounded-full cursor-pointer">
                     <p className="text-white cursor-pointer">{btnName}</p>
                 </div>
             </div>
-            <div className="grid grid-cols-3 mt-2 divide-x divide-gray-500">
-                {bannerItems.map((bannerItem, index)=>{
-                    return <div key={index} className={`${index%2===1 ? "text-center" : ""} ${index===2 ? "text-right" : ""}`}>
-                        <p className="text-[40px] font-bold">{bannerItem.statistic}</p>
-                        <p className="text-base">{bannerItem.statisticName}</p>
+            <div className="grid md:grid-cols-3 grid-cols-2 mt-2 md:divide-x md:divide-gray-400 gap-4">
+                {bannerItems.map((bannerItem, index) => {
+                    return <div key={index} className="text-center first:text-center last:text-center last:col-span-2
+                                                        md:col-span-1 md:last:col-span-1 md:last:text-right md:first:text-left
+                                                        max-md:even:border-l-2 even:border-gray-400
+                                                        ">
+                        <p className="md:text-[40px] text-3xl font-bold">{bannerItem.statistic}</p>
+                        <p className="md:text-base text-xs">{bannerItem.statisticName}</p>
                     </div>
                 })}
             </div>
         </div>
-        <div></div>
+        <div className="col-span-2 md:col-span-1">
+            <Image
+                src="/images/banner-img.jpg"
+                alt="banner-img"
+                width={656}
+                height={663}
+                className="w-full"
+            />
+        </div>
 
     </div>
 }
